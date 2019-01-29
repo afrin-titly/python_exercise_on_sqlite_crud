339f3dea50efd49f0711edd95dc23cb264892391
afrin <afrin.deeba11114@gmail.com>
2019-01-29 18:02:55 +0900

    get gmail user customers

---
+++ database/04_get_all_gmail_user_customers.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+# create database connection
+db = sqlite3.connect('employee_basic_crud.db')
+cursor = db.cursor()
+
+# write your query here
+query = ''' SELECT FirstName,lastname, email
+                FROM customers WHERE email LIKE '%gmail%' '''
+
+
+try:
+    cursor.execute(query)
+    all_rows = cursor.fetchall()
+    for row in all_rows:
+        print(row)
+except:
+    print('Something wrong in your query')
+
+# close database connection
+db.close()
