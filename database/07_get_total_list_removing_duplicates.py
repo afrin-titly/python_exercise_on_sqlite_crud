8ffb31a99c7589234fa61fa9af26c9082f8dd435
afrin <afrin.deeba11114@gmail.com>
2019-01-29 18:18:14 +0900

    get total list removing duplicates

---
+++ database/07_get_total_list_removing_duplicates.py
@@ -0,0 +1,22 @@
+# import sqlite
+import sqlite3
+
+# create database connection
+db = sqlite3.connect('employee_basic_crud.db')
+cursor = db.cursor()
+
+# write your query here
+query = ''' SELECT DISTINCT total
+                FROM invoices'''
+
+
+try:
+    cursor.execute(query)
+    all_rows = cursor.fetchall()
+    for row in all_rows:
+        print(row)
+except:
+    print('Something wrong in your query')
+
+# close database connection
+db.close()
